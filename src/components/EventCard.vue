87474607d853f16aab314938c5a36beaf671feed
maxwellharon <maxwellharon54@gmail.com>
2019-12-02 17:06:55 +0300

    auth

---
+++ src/components/EventCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="event-card">
+    <div class="card">
+      <div class="card-content">
+        <h2 class="is-size-4 has-text-weight-bold">{{ event.name }}</h2>
+        <small class="event-date">{{ event.date }}</small>
+        <span>{{ event.location }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'event'
+  ]
+}
+</script>
